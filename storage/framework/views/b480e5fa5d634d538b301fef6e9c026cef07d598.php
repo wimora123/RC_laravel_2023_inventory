fccea525a02739f7f167e4b497f01f178a7e0bf5
wimora123 <wimorasarwindo@yahoo.com>
2023-10-14 14:01:23 +0700

    first commit

---
+++ storage/framework/views/b480e5fa5d634d538b301fef6e9c026cef07d598.php
@@ -0,0 +1,6 @@
+<h1>Halaman dashboard</h1>
+
+<a href="<?php echo e(route('logout')); ?>">
+    <button>Keluar</button>
+</a>
+<?php /**PATH C:\xampp8\htdocs\RecoachingLaravel2023-LaravelInventory\resources\views/dashboard.blade.php ENDPATH**/ ?>
